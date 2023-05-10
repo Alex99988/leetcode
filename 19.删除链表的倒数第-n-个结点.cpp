b8eff5d6045916b92d35ff0411ab680b01a00228
zkk <zking1998@163.com>
2023-05-10 16:46:19 +0800

    510

--- 19.删除链表的倒数第-n-个结点.cpp
+++ 19.删除链表的倒数第-n-个结点.cpp
@@ -25,11 +25,30 @@ using namespace std;
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-//利用双指针法进行解决
+ //利用双指针法进行解决
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
+        ListNode* dummhead = new ListNode(0);
+        dummhead->next = head;
+        ListNode* fast = dummhead;
+        ListNode* slow = dummhead;
+        //向后走n步数
+        while (n-- && (fast != NULL)) {
+            fast = fast->next;
+        }
+        //再走一步
+        fast = fast->next;
+        //同时再次移动slow和fast
+        while (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode* tmp = slow->next->next;
+        slow->next = tmp;
+        return dummhead->next;
+
     }
 };
 // @lc code=end
